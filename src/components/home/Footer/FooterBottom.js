40c45aa3061f363eecf01c8e93e25f6934a6997a
Shreejan Prajapati <newapasa7@gmail.com>
2024-02-29 13:18:43 +0545

    first commit

--- src/components/home/Footer/FooterBottom.js
+++ src/components/home/Footer/FooterBottom.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineCopyright } from "react-icons/ai";
+// import { AiOutlineCopyright } from "react-icons/ai";
 
 const FooterBottom = () => {
   return (
@@ -7,7 +7,8 @@ const FooterBottom = () => {
       <div className="max-w-container mx-auto border-t-[1px] pt-10 pb-20">
         <p className="text-titleFont font-normal text-center flex md:items-center justify-center text-lightText duration-200 text-sm">
           <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
-          Copyright 2024 | PASA STORE | All Rights Reserved by Shreejan & Sameer | Powered by ABC.com
+            Copyright 2024 | PASA STORE | All Rights Reserved by Shreejan &
+            Sameer | Powered by ABC.com
           </span>
         </p>
       </div>
